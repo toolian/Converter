a02edfb05cc232a67ca9ef9c2ad373ac8c847925
toolian <toolian919@gmail.com>
2018-01-09 21:28:51 +0300

    connected methods ModulCurrency

--- form.php
+++ form.php
@@ -1,27 +1,26 @@
 <?php
-require_once 'courses.php';
-require_once 'DataBase.php';
-
-function gerKurs()
-    {	
+//require_once 'courses.php';
+require_once 'ModulCurrency.php';
 $input = isset($_GET['input'])? $_GET['input']:"Ошибка";
  	
 $currency = isset($_GET['kurs'])? $_GET['kurs']:"Ошибка";
  
-$currency2 = isset($_GET['kurs2'])? $_GET['kurs2']:"Ошибка"; 
+$currency2 = isset($_GET['kurs2'])? $_GET['kurs2']:"Ошибка";
+
+    function gerKurs($input, $currency, $currency2){
+	    $db =  new ModulCurrency();
+        $courses = $db->getAll('value');	
+        $result = $courses[$currency] * $input/ $courses[$currency2];
+        if(empty($result)){
+           echo "Введи значение";
+        }else{
+           return $result;	
+             }
 	
-$db =  new DataBase();
-$courses = $db->Select();
-	
-$result = $courses[$currency] * $input/ $courses[$currency2];
-if(empty($result)){
-    echo "Введи значение";
-	}else{
-	return $result;	
-	}
+
 }
 	
-$result = gerKurs();
+$result = gerKurs($input, $currency, $currency2);
 $result = empty($result)? "Ошибка":header("Location: index.php?result=$result");
 
 
