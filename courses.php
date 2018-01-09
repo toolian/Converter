097f7774cf08fe479a617608e6bf63983a6561be
toolian <toolian919@gmail.com>
2018-01-09 21:27:18 +0300

    connected methods ModulCurrency

--- courses.php
+++ courses.php
@@ -1,34 +1,43 @@
 <?php
-require_once 'DataBase.php';
-class WorkCourses{
-
-
-
-function UpdateRow(){
-$date = date("d/m/Y");
-$content = simplexml_load_file("https://www.cbr.ru/scripts/XML_daily.asp?date_req=".$date);
+require_once 'ModulCurrency.php';
+class WorkCourses
+{   public function UpdateRow(){
+	    $date = date("d/m/Y");
+	    $content = simplexml_load_file("https://www.cbr.ru/scripts/XML_daily.asp?date_req=".$date);
+	    $funtId = 826;
+        $usdId = 840;
+	    $eurId = 978;
+	    $cadId = 124;
+        foreach($content->Valute as $cur){ 
+	        if($cur->NumCode == $funtId){
+               $gbp = str_replace(",", ".", $cur->Value);
+            } 
+	        if($cur->NumCode == $usdId){
+	           $usd = str_replace(",", ".", $cur->Value);
+	        } 
+	        if($cur->NumCode == $eurId){
+	           $eur = str_replace(",", ".", $cur->Value);
+	        } 
+            if($cur->NumCode == $cadId){
+	           $cad = str_replace(",", ".", $cur->Value);
+	        } 
+	    }
 	
-foreach($content->Valute as $cur){ 
-	if($cur->NumCode == 826){
-       $gbp = str_replace(",", ".", $cur->Value);
-       } 
-	if($cur->NumCode == 840){
-	   $usd = str_replace(",", ".", $cur->Value);
-	   } 
-	if($cur->NumCode == 978){
-	   $eur = str_replace(",", ".", $cur->Value);
-	   } 
-    if($cur->NumCode == 124){
-	   $cad = str_replace(",", ".", $cur->Value);
-	   } 
-	}
+	    $upd = new ModulCurrency();
+	    $currency = array(
+	    'USD'=>array('value'=>$usd, 'name'=>'USD'),
+	    'EUR'=>array('value'=>$eur, 'name'=>'EUR'),
+	    'GBP'=>array('value'=>$gbp, 'name'=>'GBP'),
+	    'CAD'=>array('value'=>$cad, 'name'=>'CAD'));
 	
-$db =  new DataBase();
-$updateCad = $db->Update('CAD', $cad);
-$updateEur = $db->Update('EUR', $eur);
-$updateUsd = $db->Update('USD', $usd);
-$updateGbp = $db->Update('GBP', $gbp);
-}
+        $eur = $upd->UpdateByName($currency['EUR']);
+	    $usd = $upd->UpdateByName($currency['USD']);
+	    $cad = $upd->UpdateByName($currency['CAD']);
+	    $gbp = $upd->UpdateByName($currency['GBP']);
+
+
+    }
+	
 
 }
 
