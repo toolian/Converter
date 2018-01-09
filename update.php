057d97a1e5692b016c7a88ee0d882e72080089b1
toolian <toolian919@gmail.com>
2018-01-09 21:30:07 +0300

    connected methods ModulCurrency

--- update.php
+++ update.php
@@ -1,24 +1,28 @@
 <?php  
 require_once 'courses.php';
-if(isset($_GET['sbmt'])){
-	$upd = new WorkCourses();
-	$update = $upd->UpdateRow();
-
-        $db =  new DataBase();
-		$usd = $db->selectOne('USD');
+    if(isset($_GET['sbmt'])){
+	   $upd = new WorkCourses();
+	   $update = $upd->UpdateRow();
+	   $currency = array(
+	   '0'=>array('name'=>'USD'),
+	   '1'=>array('name'=>'EUR'),
+	   '2'=>array('name'=>'GBP'),
+	   '3'=>array('name'=>'CAD'));
+       $work = new ModulCurrency();
+		$usd = $work->getByName($currency['0']);
 		$result_usd = current($usd);
 		
-		$eur = $db->selectOne('EUR');
+		$eur = $work->getByName($currency['1']);
 		$result_eur = current($eur);
 		
-		$gbp = $db->selectOne('GBP');
+		$gbp = $work->getByName($currency['2']);
 		$result_gbp = current($gbp);
 		
-		$cad = $db->selectOne('CAD');
+		$cad = $work->getByName($currency['3']);
 		$result_cad = current($cad);
 		
 		header("Location: index.php?usd=$result_usd&eur=$result_eur&gbp=$result_gbp&cad=$result_cad");
-		}
+	}
 		
 
 
