d92a6241be585e280c2b13ee471f8aed5d4e7d78
toolian <toolian919@gmail.com>
2018-01-10 22:24:28 +0300

    ='' chandet to =0

--- DataBase.php
+++ DataBase.php
@@ -6,55 +6,52 @@ class DataBase
 	//подключение
 
 	public function __construct($username = "root", $password = "", $host = "localhost", $dbname = "courses", $options = []){
-        $this->isConn = TRUE;
-        try {
-            $this->datab = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
-            $this->datab->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->datab->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            throw new Exception($e->getMessage());
-        }
-        
-    }
+		$this->isConn = TRUE;
+		try {
+			$this->datab = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
+			$this->datab->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->datab->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			} catch (PDOException $e) {
+			throw new Exception($e->getMessage());
+		}
+
+	}
 	//отключение бд
 	/*public function Disconnect(){
-        $this->datab = NULL;
-        $this->isConn = FALSE;
+		$this->datab = NULL;
+		$this->isConn = FALSE;
 	*/
-	public function getBySql($sql, $param){
+	public function getBySql($sql){
 		
 		$sth = $this->datab->prepare($sql);
-		
-		
-		$sth->bindValue(1, $param, PDO::PARAM_STR);
-		
 		$sth->execute();
-        $courses = $sth->fetchAll(PDO::FETCH_COLUMN);
+		$courses = $sth->fetchAll(PDO::FETCH_ASSOC);
 		return $courses;
 		
 	}
 	
-	public function getOneBySql($sql, $param){
-        $sth = $this->datab->prepare($sql);
-		$i='';
+	public function paramcycle($param, $sth){
+		$i='0';
 		foreach($param as $key=>$value){
 			$i++;
 			$sth->bindValue($i, $value, PDO::PARAM_STR);
 		}
-        $sth->execute();
-        $courses = $sth->fetch(PDO::FETCH_ASSOC);
+		return $sth;
+	}
+	
+	public function getOneBySql($sql, $param){
+		$sth = $this->datab->prepare($sql);
+		$sth = $this->paramcycle($param, $sth);
+		$sth->execute();
+		$courses = $sth->fetch(PDO::FETCH_ASSOC);
 		return $courses;
 		}
-    		
+
 	public function executeSql($sql, $param){
-        $sth = $this->datab->prepare($sql);
-		$i="";
-        foreach($param as $key=>$value){
-			$i++;
-		    $sth->bindValue($i, $value, PDO::PARAM_STR);
-		}
-        $sth->execute();
-    }
+		$sth = $this->datab->prepare($sql);
+		$sth = $this->paramcycle($param, $sth);
+		$sth->execute();
+	}
 
 	
 }
