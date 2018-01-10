6d618f210ff9d3894ad8133493abfbaa08ea5b9f
toolian <toolian919@gmail.com>
2018-01-10 22:34:00 +0300

    removed array

--- update.php
+++ update.php
@@ -8,18 +8,18 @@ require_once 'courses.php';
 	   '1'=>array('name'=>'EUR'),
 	   '2'=>array('name'=>'GBP'),
 	   '3'=>array('name'=>'CAD'));
-       $work = new ModulCurrency();
-		$usd = $work->getByName($currency['0']);
-		$result_usd = current($usd);
+       $work = new ModelCurrency();
+		$usd = $work->getByName('USD');
+		$result_usd = array_pop($usd);
 		
-		$eur = $work->getByName($currency['1']);
-		$result_eur = current($eur);
+		$eur = $work->getByName('EUR');
+		$result_eur = array_pop($eur);
 		
-		$gbp = $work->getByName($currency['2']);
-		$result_gbp = current($gbp);
+		$gbp = $work->getByName('GBP');
+		$result_gbp = array_pop($gbp);
 		
-		$cad = $work->getByName($currency['3']);
-		$result_cad = current($cad);
+		$cad = $work->getByName('CAD');
+		$result_cad = array_pop($cad);
 		
 		header("Location: index.php?usd=$result_usd&eur=$result_eur&gbp=$result_gbp&cad=$result_cad");
 	}
