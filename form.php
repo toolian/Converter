36c42127b888d87841f46f8d441c405aabb01681
toolian <toolian919@gmail.com>
2018-01-10 22:32:28 +0300

    removed echo

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <?php
 //require_once 'courses.php';
-require_once 'ModulCurrency.php';
+require_once 'ModelCurrency.php';
 $input = isset($_GET['input'])? $_GET['input']:"Ошибка";
  	
 $currency = isset($_GET['kurs'])? $_GET['kurs']:"Ошибка";
@@ -8,11 +8,11 @@ $currency = isset($_GET['kurs'])? $_GET['kurs']:"Ошибка";
 $currency2 = isset($_GET['kurs2'])? $_GET['kurs2']:"Ошибка";
 
     function gerKurs($input, $currency, $currency2){
-	    $db =  new ModulCurrency();
+	    $db =  new ModelCurrency();
         $courses = $db->getAll('value');	
         $result = $courses[$currency] * $input/ $courses[$currency2];
         if(empty($result)){
-           echo "Введи значение";
+           var_dump("Введи значение");
         }else{
            return $result;	
              }
