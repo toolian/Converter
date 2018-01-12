ec02a00112bd3c00deaebde86fb8663f658d1b47
toolian <toolian919@gmail.com>
2018-01-12 21:54:44 +0300

    added identation and changed method update

--- ModelCurrency.php
+++ ModelCurrency.php
@@ -6,44 +6,47 @@ class ModelCurrency
 		$db = new DataBase();
 		return $db;
 	}
-    public function getByName($name){
+	public function getByName($name){
 		$param = array('name'=>$name);
-        $sql = "SELECT name, value FROM currency WHERE name=?";
-        $select = $this->ClassDB();
-        $result = $select->getOneBySql($sql ,$param);
+		$sql = "SELECT name, value FROM currency WHERE name=?";
+		$select = $this->ClassDB();
+		$result = $select->getOneBySql($sql ,$param);
 		return $result;
 		}
 		
 	public function getAll(){
-        $sql = "SELECT name, value FROM currency";
-        $select = $this->ClassDB();
-        $result = $select->getBySql($sql);
+		$sql = "SELECT * FROM currency";
+		$select = $this->ClassDB();
+		$result = $select->getBySql($sql);
 		return $result;
-	    }
+		
+		
+		}
 	
-	public function Update($param){
-        $sql = "UPDATE currency set value= ? where name= ?";
-        $update= $this->ClassDB();
-        $result = $update->executeSql($sql,$param);
-        }
-    public function deleteById($id){
-        $sql = "DELETE FROM currency WHERE id=?";
-        $del = $this->ClassDB();
-        $result = $del->executeSql($sql,$id);
-        }
+	public function Update($value, $name){
+		$sql = "UPDATE currency set value= ? where name= ?";
+		$param = array ('value'=>$value, 'name'=>$name);
+		$update= $this->ClassDB();
+		$result = $update->executeSql($sql,$param);
+		}
+	public function deleteById($id){
+		$sql = "DELETE FROM currency WHERE id=?";
+		$del = $this->ClassDB();
+		$result = $del->executeSql($sql,$id);
+		}
 	
 	public function deleteByName($name){
-        $sql = "DELETE FROM currency WHERE name=?";
-        $del = $this->ClassDB();
-        $result = $del->executeSql($sql,$name);
-        }
+		$sql = "DELETE FROM currency WHERE name=?";
+		$del = $this->ClassDB();
+		$result = $del->executeSql($sql,$name);
+		}
 	public function save($value){
-        $sql = "INSERT INTO currency SET name=?, value=?";
-        $del = $this->ClassDB();
-        $result = $del->executeSql($sql,$value);
-        }
+		$sql = "INSERT INTO currency SET name=?, value=?";
+		$del = $this->ClassDB();
+		$result = $del->executeSql($sql,$value);
+		}
 		
 		
-      }
+		}
 
 
