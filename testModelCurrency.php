3a120750072a1706eca3f09c719d264c61be3e27
toolian <toolian919@gmail.com>
2018-01-12 21:52:51 +0300

    added identation

--- testModelCurrency.php
+++ testModelCurrency.php
@@ -1,80 +1,72 @@
 <?php
 require_once 'Modelcurrency.php';
-//массив для UpdateByName
-$currency = array(
-	    'USD'=>array('value'=>'55', 'name'=>'USD'),
-	    'EUR'=>array('value'=>'65', 'name'=>'EUR'),
-	    'GBP'=>array('value'=>'70', 'name'=>'GBP'),
-	    'CAD'=>array('value'=>'45', 'name'=>'CAD'));
-//массив для GetByName
+
 
 $test = new Modelcurrency();
 $getAll = $test->getAll();
-
 //GetAll
 if(count($getAll)=='5'){
 	echo "GETALL OK<br>";
 }else{
 	echo "ошибка";
-     }
+	}
 //GetByName	 
 $getByUSD = $test->getByName('USD');
-var_dump($getByUSD);
-if(count($getByUSD)=='1'){
+if(count($getByUSD)=='2'){
 	echo "getByName: USD OK";
 }else{
 	echo "USD ошибка";
-     }
+	}
 $getByEUR = $test->getByName('EUR');
-if(count($getByEUR)=='1'){
+if(count($getByEUR)=='2'){
 	echo ", EUR OK";
 }else{
 	echo ", EUR ошибка";
-     }
+	}
 $getByGBP = $test->getByName('GBP');
-if(count($getByGBP)=='1'){
+if(count($getByGBP)=='2'){
 	echo ", GBP OK";
 }else{
 	echo ", GBP ошибка";
-     }
+	}
 $getByCAD = $test->getByName('CAD');
-if(count($getByCAD)=='1'){
+if(count($getByCAD)=='2'){
 	echo ", CAD OK";
 }else{
 	echo ", CAD ошибка";
-     }
+	}
 $getByRUB = $test->getByName('RUB');
-if(count($getByRUB)=='1'){
+if(count($getByRUB)=='2'){
 	echo ", RUB OK<br>";
 }else{
 	echo ", RUB ошибка<br>";
-     }
+	}
 //UpdateByName	 
-$eur = $test->Update($currency['EUR']);
-$usd = $test->Update($currency['USD']);
-$cad = $test->Update($currency['CAD']);
-$gbp = $test->Update($currency['GBP']);
-if(current($getByUSD) == '55'){
+$eur = $test->Update(65, 'EUR');
+$usd = $test->Update(55, 'USD');
+$cad = $test->Update(45, 'CAD');
+$gbp = $test->Update(70, 'GBP');
+if(array_pop($getByUSD) == '55'){
 	echo "UpdateByName : USD OK";
 }else{
-    echo " UpdateByName : USD ошибка";
-     }
-if(current($getByEUR) == '65'){
+	echo " UpdateByName : USD ошибка";
+	}
+if(array_pop($getByEUR) == '65'){
 	echo ", EUR OK";
 }else{
-    echo ", EUR ошибка";
-     }
-if(current($getByGBP) == '70'){
+	echo ", EUR ошибка";
+	}
+if(array_pop($getByGBP) == '70'){
 	echo ", GBP OK";
 }else{
-    echo " UpdateByName : GBP ошибка";
-     }
-if(current($getByCAD) == '45'){
+	echo " UpdateByName : GBP ошибка";
+	}
+if(array_pop($getByCAD) == '45'){
 	echo ", CAD OK";
 }else{
-    echo ", CAD ошибка";
-     }
-	 
+	echo ", CAD ошибка";
+	}
+	
 	
 
 
